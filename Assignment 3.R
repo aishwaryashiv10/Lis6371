2f6bc95fcdd8041aa61e2b3b496fa17285b472cd
aishwaryashiv10 <122128832+aishwaryashiv10@users.noreply.github.com>
2023-01-29 23:00:52 -0500

    Update Assignment 3.R

--- Assignment 3.R
+++ Assignment 3.R
@@ -4,10 +4,29 @@ NBC_politicalpollresults <- c(12, 75, 43, 19, 1, 21, 19)
 
 results <- cbind(Name, ABC_politicalpollresults, NBC_politicalpollresults)
 results
+ Name      ABC_politicalpollresults NBC_politicalpollresults
+[1,] "Jeb"     "4"                      "12"                    
+[2,] "Donald"  "62"                     "75"                    
+[3,] "Ted"     "51"                     "43"                    
+[4,] "Marco"   "21"                     "19"                    
+[5,] "Carly"   "2"                      "1"                     
+[6,] "Hillary" "14"                     "21"                    
+[7,] "Berine"  "15"                     "19"   
 
 results.df <- data.frame(Name,  ABC_politicalpollresults, NBC_politicalpollresults)
 results.df
+Name ABC_politicalpollresults NBC_politicalpollresults
+1     Jeb                        4                       12
+2  Donald                       62                       75
+3     Ted                       51                       43
+4   Marco                       21                       19
+5   Carly                        2                        1
+6 Hillary                       14                       21
+7  Berine                       15                       19
 
 mean(ABC_politicalpollresults)
+[1] 24.14286
 mean(NBC_politicalpollresults)
+[1] 27.14286
 mean(ABC_politicalpollresults + NBC_politicalpollresults)
+[1] 51.28571
