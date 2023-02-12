2a1d2d6697fb90e88bf5b71bd43f0d38006475fd
aishwaryashiv10 <122128832+aishwaryashiv10@users.noreply.github.com>
2023-02-12 15:57:07 -0500

    Add files via upload

---
+++ Assignment 5.R
@@ -0,0 +1,27 @@
+A <- matrix(1:100, nrow=10)  
+B <- matrix(1:1000, nrow=10)
+
+# Transpose A and B  
+t(A)
+t(B)
+
+# Create vectors
+a <- c(1:100)
+b <- c(1:1000)
+
+X <- A*a
+Y <- B*b
+
+a=c(1:100)
+b=c(1:1000)
+
+A*a %*%
+B*b %*%
+  
+S=matrix(A, nrow=2)
+det(S)
+
+det(A)
+det(B)
+det(X)
+det(Y)
