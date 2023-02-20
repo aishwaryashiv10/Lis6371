ab5ad6961ed6a4f2d2a203d1871aabc7e695c8e6
aishwaryashiv10 <122128832+aishwaryashiv10@users.noreply.github.com>
2023-02-19 23:21:12 -0500

    Add files via upload

---
+++ Assignment 6.R
@@ -0,0 +1,11 @@
+# Part 1 
+A <- matrix(c(2,0,1,3), ncol=2)
+B <- matrix(c(5,2,4,-1), ncol=2)
+A + B
+A - B
+
+# Part 2
+diag(c(4,1,2,3))
+
+# Part 3
+matrix(c(3,2,2,2,2,1,3,0,0,0,1,0,3,0,0,1,0,0,3,0,1,0,0,0,3), nrow = 5, byrow = FALSE)
