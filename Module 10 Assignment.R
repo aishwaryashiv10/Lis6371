43a6c5212f903c9ce9836a4353d3813c1ebd2233
aishwaryashiv10 <122128832+aishwaryashiv10@users.noreply.github.com>
2023-03-20 17:15:48 -0400

    Add files via upload

---
+++ Module 10 Assignment.R
@@ -0,0 +1,13 @@
+install.packages("devtools")
+Package: 'checkpoint'
+Title: Able to save results from R console into a file. 
+Version: 0.0.0.9000
+Authors@R: "Aishwarya Shivakumar <ashivakumar@usf.edu> [aut, cre]"
+Description: Often times I face the difficulty of not being able to save all my 
+results from my R console to a file to refer back to it later. However, with my 
+new package it will be able to save all the results after running the R script 
+into a file and will be able to update the result for each line of code that is 
+ran incase you want to edit it and rerun it. 
+Depends: R (>= 2022.12.0+353)
+License: CC0 1.0
+LazyData: true
