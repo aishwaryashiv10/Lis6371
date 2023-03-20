08a989e95e6e79ff1c97950fef1e3f7fb45b6de3
aishwaryashiv10 <122128832+aishwaryashiv10@users.noreply.github.com>
2023-03-20 17:19:25 -0400

    Update Module 10 Assignment.R

--- Module 10 Assignment.R
+++ Module 10 Assignment.R
@@ -6,8 +6,7 @@ Authors@R: "Aishwarya Shivakumar <ashivakumar@usf.edu> [aut, cre]"
 Description: Often times I face the difficulty of not being able to save all my 
 results from my R console to a file to refer back to it later. However, with my 
 new package it will be able to save all the results after running the R script 
-into a file and will be able to update the result for each line of code that is 
-ran incase you want to edit it and rerun it. 
+into a file and will be able to update the result for each line of code incase you want to edit it and rerun it. 
 Depends: R (>= 2022.12.0+353)
 License: CC0 1.0
 LazyData: true
