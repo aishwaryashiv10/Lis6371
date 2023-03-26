b07a9ac257cf31f147fd5459448496c0aecd8d76
aishwaryashiv10 <122128832+aishwaryashiv10@users.noreply.github.com>
2023-03-26 14:17:42 -0400

    Add files via upload

---
+++ Assignment 11.r
@@ -0,0 +1,26 @@
+## Original Code
+tukey_multiple <- function(x) {
+  outliers <- array(TRUE,dim=dim(x))
+  for (j in 1:ncol(x))
+  {
+    outliers[,j] <- outliers[,j] && tukey.outlier(x[,j])
+  }
+  outlier.vec <- vector(length=nrow(x))
+  for (i in 1:nrow(x))
+  { outlier.vec[i] <- all(outliers[i,]) } return(outlier.vec) }
+
+
+#DeBugged Code
+tukey_multiple <- function(x) {
+  outliers <- array(TRUE, dim=dim(x))
+  for (j in 1:ncol(x)) {
+    outliers[,j] <- outliers[,j] && #tukey.outlier(x[,j])  ## tukey.outlier does not exist
+      }
+  outlier.vec <- vector(length=nrow(x))
+  for (i in 1:nrow(x)) {
+    outlier.vec[i] <- all(outliers[i,])
+  }
+  return(outlier.vec) 
+}
+
+
